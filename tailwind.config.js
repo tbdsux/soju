6865eab3d7678b0ed1df52271ec6f9133d098674
TheBoringDude <iamcoderx@gmail.com>
2021-03-10 21:03:15 +0800

    initial design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+    },
+    fontFamily: {
+      sans: ['"Recursive"', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
